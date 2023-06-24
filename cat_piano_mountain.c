be66f1fbd109c2671a9b70cb837ad24a43c51199
moisepierre69 <136640043+moisepierre69@users.noreply.github.com>
2023-06-24 17:44:25 +0700

    Fix memory management issues

---
+++ cat_piano_mountain.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main (void)
+{
+     // Print out heading
+     printf("My Sustainability Journey\n\n");
+ 
+     // Print out first section
+     printf("I started my sustainability journey by educating myself on the importance of adopting sustainable habits. I read books on the environmental impacts of our lifestyles, watched documentaries and engaged with local experts to learn more. I found that living a more sustainable lifestyle has many benefits, both for me and for the planet!\n\n");
+
+     // Print out second section
+     printf("I then started to take action. I took small steps at first: swapping out my old lightbulbs for energy-efficient LED bulbs, walking or using public transport instead of driving, and adopting mindful shopping habits. I found that these small changes really made a big difference to my impact on the environment.\n\n");
+
+     // Print out third section
+     printf("My next step was to focus on reducing my waste. I started by researching ways to reduce everyday waste, such as using reusable containers and grocery bags, investing in sustainable products, and avoiding single-use plastics whenever possible. I also began to compost my food waste which allowed me to divert it from landfills and put it to better use.\n\n");
+
+     // Print out fourth section
+     printf("I also started exploring eco-friendly products that can help reduce my environmental impact. I researched the benefits of using renewable energy sources, such as solar and wind power, as well as switching to certified organic and sustainable food sources. I also adopted green cleaning methods to reduce the amount of toxic chemicals I was using.\n\n");
+
+     // Print out fifth section
+     printf("As I continued on my sustainability journey, I started to get more involved in my local community. I began volunteering at local green spaces, taking part in beach clean-ups, and organizing community events to raise awareness about the importance of living a sustainable lifestyle.\n\n");
+
+     // Print out sixth section
+     printf("My sustainability journey has continued to evolve over the years. I am constantly learning more about ways to reduce my environmental impact and staying up to date with the latest innovations in sustainable living. I am proud to say that my efforts have made a difference, both on an individual and collective level.\n\n");
+
+     return 0;
+}
